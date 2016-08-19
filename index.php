19529c07880cb3f3ca250ea57e63c6803dfd9c07
Chalrey Deker <access-delined@list.ru>
2016-08-19 08:18:31 +0300

    index created

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel='StyleSheet' href='/style.css' type='text/css' />
+	<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+	<meta name=viewport content="width=device-width, initial-scale=1" />
+	<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+	<script src='/js/jquery-1.4.2.min.js'></script>
+	<script src='/js/main.js'></script>
+</head>
+<body>
+
+</body>
+</html>
